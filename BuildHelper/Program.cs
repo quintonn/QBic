fe9b6e6d8de86999e92861d769b2ae09d7fe570b
quintonn <none@none>
2015-11-29 22:47:08 +0400

    Added space for sub-menu to menu bar

--- BuildHelper/Program.cs
+++ BuildHelper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +19,23 @@ namespace BuildHelper
             SquishIt.Framework.CSS.CSSBundle x = new SquishIt.Framework.CSS.CSSBundle();
             x.AddDirectory(currentDir, true);
 
+            var cssFileName = webPath + "styles.min.css";
+            if (File.Exists(cssFileName))
+            {
+                File.Delete(cssFileName);
+            }
+            var jsFileName = webPath + "scripts.min.js";
+            if (File.Exists(jsFileName))
+            {
+                File.Delete(jsFileName);
+            }
 
-            var result = x.Render(webPath + "styles.min.css");
+            var result = x.Render(cssFileName);
             Console.WriteLine(result);
 
             SquishIt.Framework.JavaScript.JavaScriptBundle j = new SquishIt.Framework.JavaScript.JavaScriptBundle();
             j.AddDirectory(currentDir, true);
-            result = j.Render(webPath + "scripts.min.js");
+            result = j.Render(jsFileName);
 
             Console.WriteLine(result);
         }
