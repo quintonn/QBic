95375ea40ca67187414ea7b3570d8ffc0991f7b8
quintonn <none@none>
2015-12-23 19:51:43 +0400

    Added custom input types for data source combo box and enum based combo box

---
+++ WebsiteTemplate/Menus/InputItems/DataSourceComboBoxInput.cs
@@ -0,0 +1,67 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using WebsiteTemplate.Data;
+using WebsiteTemplate.Models;
+
+namespace WebsiteTemplate.Menus.InputItems
+{
+    public class DataSourceComboBoxInput<T> : ComboBoxInput where T : BaseClass
+    {
+        /// <summary>
+        /// This creates a <see cref="WebsiteTemplate.Menus.InputItems.ComboBoxInput"> item where the source is obtained
+        /// from the database for the type T.
+        /// </summary>
+        /// <param name="name">Input name to be used by process method.</param>
+        /// <param name="label">Label to be displayed on user interface to user.</param>
+        /// <param name="keyFunc">Function to return a property of T or a value to use as the key in the dictionary to use for the list source.</param>
+        /// <param name="valueFunc">Function to return a property of T or a value to use as the value in the dictionary.</param>
+        /// <param name="defaultValue">Default value to use for this field. Should match a value from returned by keyFunc or be null.</param>
+        /// <param name="tabName">The name of the tab to appear on the user interface, or null if only 1 tab is required.</param>
+        /// <param name="whereClause">An optional where clause to pass into the NHibernate query.</param>
+        /// <param name="orderByClause">An optional Order by clause to pass into the NHibernate query.</param>
+        /// <param name="orderByAsc">If an order by clause is provided, this will set order direction.</param>
+        public DataSourceComboBoxInput(string name, 
+                                       string label,
+                                       Func<T, string> keyFunc,
+                                       Func<T, object> valueFunc,
+                                       object defaultValue = null, 
+                                       string tabName = null, 
+                                       Expression<Func<T, bool>> whereClause = null,
+                                       Expression<Func<T, object>> orderByClause = null,
+                                       bool orderByAsc = true)
+            : base(name, label, defaultValue, tabName)
+        {
+            var store = new DataStore();
+            using (var session = store.OpenSession())
+            {
+                var queryOver = session.QueryOver<T>();
+                if (whereClause != null)
+                {
+                    ///  Leave this for references - #LambdaExpression
+                    ///  var expression = Expression.Lambda<Func<bool>>(Expression.Call(whereClause.Method));
+                    ///  queryOver = queryOver.Where(expression);
+                    queryOver = queryOver.Where(whereClause);
+                }
+                if (orderByClause != null)
+                {
+                    if (orderByAsc)
+                    {
+                        queryOver = queryOver.OrderBy(orderByClause).Asc;
+                    }
+                    else
+                    {
+                        queryOver = queryOver.OrderBy(orderByClause).Desc;
+                    }
+                }
+                var list = queryOver.List<T>();
+
+                var result = list.ToDictionary(keyFunc, valueFunc);
+                ListItems = result;
+            }
+        }
+    }
+}
---
+++ WebsiteTemplate/Menus/InputItems/EnumComboBoxInput.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web;
+
+namespace WebsiteTemplate.Menus.InputItems
+{
+    public class EnumComboBoxInput<T> : ComboBoxInput where T : struct, IConvertible
+    {
+        public EnumComboBoxInput(string name, 
+                                 string label,
+                                 bool addBlankValue = false,
+                                 //Func<FieldInfo, string> keyFunc,
+                                 //Func<FieldInfo, object> valueFunc,
+                                 Func<KeyValuePair<T, string>, bool> whereClause = null,
+                                 Func<KeyValuePair<T, string>, object> orderByClause = null,
+                                 object defaultValue = null, 
+                                 string tabName = null)
+            : base(name, label, defaultValue, tabName)
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enumerated type (enum)");
+            }
+
+            var enumValues = Enum.GetValues(typeof(T)).Cast<T>().ToList();
+            var items = enumValues.ToDictionary(e => (T)e, e => e.ToString());
+
+            //var items = fields.ToDictionary(keyFunc, valueFunc);
+            //var items = fields.ToDictionary(e => e.GetValue(null).ToString(), e => e.Name);
+            
+            if (whereClause != null)
+            {
+                items = items.Where(whereClause).ToDictionary(e => e.Key, e => e.Value);
+            }
+            if (orderByClause != null)
+            {
+                items = items.OrderBy(orderByClause).ToDictionary(e => e.Key, e => e.Value);
+            }
+
+            var listItems = items.ToDictionary(e => e.Key.ToString(), e => (object)e.Value).ToList();
+            if (addBlankValue)
+            {
+                listItems.Insert(0, new KeyValuePair<string, object>("", ""));
+            }
+
+            ListItems = listItems.ToDictionary(e => e.Key, e => e.Value);
+        }
+    }
+}
