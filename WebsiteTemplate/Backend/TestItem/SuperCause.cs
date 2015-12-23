4e94006d200de3327024b43dc73f86fd88dc0980
quintonn <none@none>
2015-12-23 19:51:55 +0400

    Testing and adding code comments

--- WebsiteTemplate/Backend/TestItem/SuperCause.cs
+++ WebsiteTemplate/Backend/TestItem/SuperCause.cs
@@ -11,5 +11,7 @@ namespace WebsiteTemplate.Backend.TestItem
         public virtual string SuperName { get; set; }
 
         public virtual bool SuperOk { get; set; }
+
+        public virtual DateTime? SuperDate { get; set; }
     }
 }
