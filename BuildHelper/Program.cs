99542ea38105d595d393a3d2570e9463e67a6019
quintonn <none@none>
2016-03-27 11:44:30 +0400

    Created new sub project that will be published to nuget

--- BuildHelper/Program.cs
+++ BuildHelper/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BuildHelper
@@ -12,12 +13,18 @@ namespace BuildHelper
     {
         static void Main(string[] args)
         {
-            var currentDir = Environment.CurrentDirectory + @"\..\";
-            var webPath = currentDir + @"Frontend\webFiles\";
+            BuildWebsiteTemplate();
+        }
+
+        private static void BuildWebsiteTemplate()
+        {
+            var currentDir = Environment.CurrentDirectory + @"\..\..\";
+            var webPath = currentDir + @"WebsiteTemplateCore\FrontEnd\";
+            //D:\Projects\WebsiteTemplate\WebsiteTemplate\Frontend
 
             //Debug.Assert(false);
             SquishIt.Framework.CSS.CSSBundle x = new SquishIt.Framework.CSS.CSSBundle();
-            x.AddDirectory(currentDir, true);
+            x.AddDirectory(currentDir + "\\WebsiteTemplate\\FrontEnd", true);
 
             var cssFileName = webPath + "styles.min.css";
             if (File.Exists(cssFileName))
@@ -34,8 +41,61 @@ namespace BuildHelper
             Console.WriteLine(result);
 
             SquishIt.Framework.JavaScript.JavaScriptBundle j = new SquishIt.Framework.JavaScript.JavaScriptBundle();
-            j.AddDirectory(currentDir, true);
+            j.AddDirectory(currentDir + "\\WebsiteTemplate\\FrontEnd", true);
             result = j.Render(jsFileName);
+
+            /// Copy html pages in Pages folder
+            var pages = Directory.GetFiles(currentDir + "WebsiteTemplate\\FrontEnd\\Pages").ToList();
+            if (!Directory.Exists(currentDir + "WebsiteTemplateCore\\FrontEnd\\Pages"))
+            {
+                Directory.CreateDirectory(currentDir + "WebsiteTemplateCore\\FrontEnd\\Pages");
+            }
+            pages.ForEach(p =>
+            {
+                var newFile = currentDir + "WebsiteTemplateCore\\FrontEnd\\Pages\\" + p.Split("\\".ToCharArray()).Last();
+                File.Copy(p, newFile, true);
+            });
+
+            /// Copy and Edit Index.html page
+            var indexPath = currentDir + "WebsiteTemplateCore\\Index.html";
+            //File.Copy(currentDir + "WebsiteTemplate\\Index.html", indexPath, true);
+
+            var data = File.ReadAllText(indexPath);
+            var regex = new Regex("<link href.*/>?");
+            var matches = regex.Matches(data);
+            for (var i = 0; i < matches.Count; i++)
+            {
+                var match = matches[i];
+                if (i == 0)
+                {
+                    data = data.Replace(match.Value, "<link href='Frontend/styles.min.css' rel='stylesheet' />");
+                }
+                else
+                {
+                    data = data.Replace(match.Value, "");
+                }
+            }
+
+            regex = new Regex("<script.*</script>?");
+            matches = regex.Matches(data);
+            for (var i = 0; i < matches.Count; i++)
+            {
+                var match = matches[i];
+                if (i == 0)
+                {
+                    data = data.Replace(match.Value, "<script src='Frontend/scripts.min.js'></script>");
+                }
+                else
+                {
+                    data = data.Replace(match.Value, "");
+                }
+            }
+
+            var lines = data.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+            lines = lines.Where(l => !String.IsNullOrWhiteSpace(l)).ToList();
+            data = String.Join("\r\n", lines.ToArray());
+
+            File.WriteAllText(indexPath, data);
 
             Console.WriteLine(result);
         }
---
+++ WebsiteTemplateCore/nuget publish.bat
@@ -0,0 +1,5 @@
+if not exist ..\NugetPackages mkdir ..\NugetPackages
+nuget pack -OutputDirectory "..\NugetPackages"  "..\WebsiteTemplateCore.csproj"
+rem -Exclude "config\*.*"
+nuget pack -OutputDirectory "..\..\..\NugetSource"  "..\WebsiteTemplateCore.csproj"
+rem nuget push "..\NugetPackages\WebsiteTemplate*.nupkg" -s http://cloudup.azurewebsites.net/ CF8F5B76-5C89-4032-AC36-B1C6DB70CA67
