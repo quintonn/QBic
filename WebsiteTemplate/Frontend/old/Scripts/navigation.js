8dc4024e76a262385d817e95cf5f69877bb7dc7b
Quintonn <q10athome@gmail.com>
2016-07-17 22:56:49 +0400

    Added old code for reference purposes
    --HG--
    branch : W3css

---
+++ WebsiteTemplate/Frontend/old/Scripts/navigation.js
@@ -0,0 +1,80 @@
+﻿var navigation = {
+
+    entryPoint: function (userInfo)
+    {
+        navigation.loadHtmlBody("mainContent", "Home.html");
+
+        navigation.getUserMenu();
+    },
+
+    getUserMenu: function ()
+    {
+        main.makeWebCall(main.webApiURL + "getUserMenu", "GET", navigation.processUserMenuResponse);
+    },
+
+    processUserMenuResponse: function (data)
+    {
+        var menuList = data;
+        // figure out what page to go to //TODO
+        menuBuilder.buildMenu(menuList);
+    },
+
+    clearSettings: function ()
+    {
+        localStorage.removeItem(main.tokenName);
+        localStorage.removeItem(main.userSettingName);
+    },
+
+    showLoginPage: function ()
+    {
+        menuBuilder.clearMenu();
+        navigation.loadHtmlBody("loginContainer", "Login.html", navigation.loadLoginPageCompleted);
+    },
+
+    loadLoginPageCompleted: function ()
+    {
+        document.getElementById('txtName').onkeypress = function (e)
+        {
+            if (e.keyCode == 13)
+            {
+                document.getElementById('txtPassword').focus();
+            }
+        };
+        document.getElementById('txtPassword').onkeypress = function (e)
+        {
+            if (e.keyCode == 13)
+            {
+                auth.doLogin();
+            }
+        };
+        document.getElementById("loginContainer").style.visibility = "visible";
+        document.getElementById("txtName").focus();
+    },
+
+    loadHtmlBody: function (nodeName, pageName, callback)
+    {
+        var url = "/frontend/pages/" + pageName + '?v=' + Math.random();// + main.version;
+        var loadCompleted = function (resp, nodeName)
+        {
+            navigation.loadHtmlBodyResponse(resp, nodeName);
+            if (callback)
+            {
+                callback();
+            }
+        };
+        main.makeWebCall(url, "GET", loadCompleted, null, nodeName);
+    },
+
+    loadHtmlBodyResponse: function (response, nodeName)
+    {
+        var div = document.getElementById(nodeName);
+        div.innerHTML = response;
+    },
+
+    getParameterByName: function (name)
+    {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+};
