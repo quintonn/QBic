c88f0f12c7668b608ef80d02740e07fea76c5547
Quintonn <q10athome@gmail.com>
2016-08-01 15:53:21 +0400

    Added ability to more easily submit LetsEncrypt challenges
    --HG--
    branch : W3css

---
+++ WebsiteTemplate/Controllers/AcmeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebsiteTemplate.Controllers
+{
+    [RoutePrefix(".well-known/acme-challenge")]
+    public class AcmeController : ApiController
+    {
+        [HttpGet]
+        [Route("{*path}")]
+        [AllowAnonymous]
+        public IHttpActionResult Test(string path)
+        {
+            //TODO: Can create class and input screen to manage this
+            return Json("successs: " + path);
+        }
+    }
+}
