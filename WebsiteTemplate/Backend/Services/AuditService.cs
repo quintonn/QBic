ec4d1a9ce5415752093b7af04e16ed3069f9d021
Quintonn <q10athome@gmail.com>
2016-08-26 18:24:16 +0400

    Added audit logging
    --HG--
    branch : W3css

--- WebsiteTemplate/Backend/Services/AuditService.cs
+++ WebsiteTemplate/Backend/Services/AuditService.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Practices.Unity;
+using System;
 using System.Threading.Tasks;
 using WebsiteTemplate.Data;
 using WebsiteTemplate.Models;
+using WebsiteTemplate.Utilities;
 
 namespace WebsiteTemplate.Backend.Services
 {
@@ -18,9 +20,40 @@ namespace WebsiteTemplate.Backend.Services
             var user = await BasicAuthentication.ControllerHelpers.Methods.GetLoggedInUserAsync() as User;
         }
 
-        public void AuditChange<T>(T item, AuditAction action) where T : BaseClass
+        public async void AuditChange<T>(T item, AuditAction action) where T : BaseClass
         {
-            var id = item.Id;
+            var user = await BasicAuthentication.ControllerHelpers.Methods.GetLoggedInUserAsync() as User;
+
+            using (var session = DataStore.OpenSession())
+            {
+                object existingItem = null;
+                if (!String.IsNullOrWhiteSpace(item.Id))
+                {
+                    existingItem = session.Get<T>(item.Id);
+                }
+
+                var auditEvent = new AuditEvent()
+                {
+                    AuditAction = action,
+                    AuditEventDateTimeUTC = DateTime.UtcNow,
+                    User = user,
+                    ObjectId = item.Id,
+                    OriginalObject = SerializeObject(existingItem),
+                    NewObject = action != AuditAction.Delete ? SerializeObject(item) : String.Empty
+                };
+                session.Save(auditEvent);
+                session.Flush();
+            }
+        }
+
+        private string SerializeObject(object item)
+        {
+            if (item == null)
+            {
+                return String.Empty;
+            }
+            var result = JsonHelper.SerializeObject(item);
+            return result;
         }
     }
 }
