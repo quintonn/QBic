2edee871ac7036b68a6ff29f96e751c8826a3da4
Quintonn <q10athome@gmail.com>
2016-09-01 23:28:07 +0400

    Add TransactionScope to core processor for all events
    --HG--
    branch : W3css

--- WebsiteTemplate/Backend/Processing/CoreProcessor.cs
+++ WebsiteTemplate/Backend/Processing/CoreProcessor.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Web.Http;
 using System.Web.Http.Results;
 using WebsiteTemplate.Controllers;
@@ -22,26 +23,29 @@ namespace WebsiteTemplate.Backend.Processing
 
         internal async Task<IHttpActionResult> Process(int eventId, HttpRequestMessage requestMessage)
         {
-            try
+            using (var scope = new TransactionScope())
             {
-                await AuditService.LogUserEvent(eventId);
-                var result = await ProcessEvent(eventId);
-
-                if (result is FileActionResult)
+                try
                 {
-                    return result as FileActionResult;
+                    await AuditService.LogUserEvent(eventId);
+                    var result = await ProcessEvent(eventId);
+
+                    if (result is FileActionResult)
+                    {
+                        return result as FileActionResult;
+                    }
+
+                    var jsonResult = new JsonResult<T>(result, JSON_SETTINGS, Encoding.UTF8, requestMessage);
+                    scope.Complete();
+                    return jsonResult;
                 }
-
-                var jsonResult = new JsonResult<T>(result, JSON_SETTINGS, Encoding.UTF8, requestMessage);
-
-                return jsonResult;
-            }
-            catch (Exception error)
-            {
-                return new BadRequestErrorMessageResult(error.Message, new DefaultContentNegotiator(), requestMessage, new List<MediaTypeFormatter>()
+                catch (Exception error)
                 {
-                    new JsonMediaTypeFormatter()
-                });
+                    return new BadRequestErrorMessageResult(error.Message, new DefaultContentNegotiator(), requestMessage, new List<MediaTypeFormatter>()
+                    {
+                        new JsonMediaTypeFormatter()
+                    });
+                }
             }
         }
     }
