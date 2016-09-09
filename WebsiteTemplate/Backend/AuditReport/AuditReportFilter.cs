afd469cae7576a2ee06e4be22babdbcdad782908
Quintonn <q10athome@gmail.com>
2016-09-09 12:46:37 +0400

    Created a basic audit report

---
+++ WebsiteTemplate/Backend/AuditReport/AuditReportFilter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebsiteTemplate.Backend.Services;
+using WebsiteTemplate.Menus;
+using WebsiteTemplate.Menus.BaseItems;
+using WebsiteTemplate.Menus.InputItems;
+using WebsiteTemplate.Utilities;
+
+namespace WebsiteTemplate.Backend.AuditReport
+{
+    public class AuditReportFilter : GetInput
+    {
+        public override string Description
+        {
+            get
+            {
+                return "Audit Report Filter";
+            }
+        }
+
+        private UserService UserService { get; set; }
+
+        public AuditReportFilter(UserService userService)
+        {
+            UserService = userService;
+        }
+
+        public override IList<InputField> InputFields
+        {
+            get
+            {
+                var list = new List<InputField>();
+
+                list.Add(new DateInput("FromDate", "From Date", DateTime.Now.Date, null, true));
+                list.Add(new DateInput("ToDate", "To Date", DateTime.Now.Date, null, true));
+
+                var users = UserService.RetrieveUsers(0, int.MaxValue, String.Empty).ToDictionary(u => u.Id, u => (object)u.UserName);
+                var listSelection = new ListSelectionInput("Users", "Users")
+                {
+                    ListSource = users
+                };
+                list.Add(listSelection);
+
+                return list;
+            }
+        }
+
+        public override EventNumber GetId()
+        {
+            return EventNumber.AuditReportFilter;
+        }
+
+        public override async Task<IList<IEvent>> ProcessAction(int actionNumber)
+        {
+            var fromDate = GetValue<DateTime>("FromDate");
+            var toDate = GetValue<DateTime>("ToDate");
+            var userIds = GetValue<List<string>>("Users");
+
+            var json = new JsonHelper();
+            json.Add("FromDate", fromDate);
+            json.Add("ToDate", toDate);
+            json.Add("UserIds", JsonHelper.SerializeObject(userIds));
+
+            return new List<IEvent>()
+            {
+                new ExecuteAction(EventNumber.CreateAuditReport, json.ToString()),
+                new CancelInputDialog(),
+            };
+        }
+    }
+}
---
+++ WebsiteTemplate/Backend/AuditReport/CreateAuditReport.cs
@@ -0,0 +1,102 @@
+﻿using DocumentGenerator.DocumentTypes;
+using DocumentGenerator.Settings;
+using DocumentGenerator.Styles;
+using MigraDoc.DocumentObjectModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NHibernate.Criterion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebsiteTemplate.Backend.Services;
+using WebsiteTemplate.Menus;
+using WebsiteTemplate.Menus.BaseItems;
+using WebsiteTemplate.Menus.InputItems;
+using WebsiteTemplate.Models;
+using WebsiteTemplate.Utilities;
+
+namespace WebsiteTemplate.Backend.AuditReport
+{
+    public class CreateAuditReport : OpenFile
+    {
+        public override string Description
+        {
+            get
+            {
+                return "Create Audit Report";
+            }
+        }
+
+        private StyleSetup StyleSetup { get; set; }
+        private DataService DataService { get; set; }
+
+        public CreateAuditReport(StyleSetup styleSetup, DataService dataService)
+        {
+            StyleSetup = styleSetup;
+            DataService = dataService;
+        }
+
+        public override async Task<FileInfo> GetFileInfo(string data)
+        {
+            var json = JsonHelper.Parse(data);
+
+            var fromDate = json.GetValue<DateTime>("FromDate");
+            var toDate = json.GetValue<DateTime>("ToDate");
+            var userIdString = json.GetValue("UserIds");
+            var userIds = JArray.Parse(userIdString).Select(u => u.ToString()).ToArray();
+
+            var document = new BasicTableLayoutDocument(StyleSetup, new DocumentSettings(DocumentType.Pdf, Orientation.Landscape));
+            document.SetDocumentTitle("Audit Report: from " + fromDate.ToShortDateString() + " to " + toDate.ToShortDateString()); //TODO: need a subheading in report
+
+            var user = await BasicAuthentication.ControllerHelpers.Methods.GetLoggedInUserAsync();
+
+            var footer = "Printed by " + user.UserName + " on " + System.DateTime.Now.ToString("yyyy-MM-dd");
+            document.SetDocumentFooter(footer);
+            document.SetSideMargin(Unit.FromCentimeter(1));
+
+            document.AddRowHeading("Date Time\t\tAction\tEntity");
+            document.AddRowHeading("Tmp", "");
+            //document.AddRowHeading("Action");
+            //document.AddRowHeading("Entity");
+            //document.AddRowHeading("Original Value");
+            //document.AddRowHeading("New Value");
+
+            using (var session = DataService.OpenSession())
+            {
+                var auditEvents = session.CreateCriteria<AuditEvent>()
+                                         .CreateAlias("User", "user")
+                                         .Add(Restrictions.Gt("AuditEventDateTimeUTC", fromDate.AddDays(-1)))
+                                         .Add(Restrictions.Lt("AuditEventDateTimeUTC", toDate.AddDays(1)))
+                                         .Add(Restrictions.In("user.Id", userIds))
+                                         .AddOrder(Order.Asc("AuditEventDateTimeUTC"))
+                                         .AddOrder(Order.Asc("EntityName"))
+                                         .List<AuditEvent>()
+                                         .ToList();
+                foreach (var audit in auditEvents)
+                {
+                    var origObj = audit.OriginalObject;
+                    var newObj = audit.NewObject.Replace(",\"", ", \"");
+                    document.AddRowUsingParams(audit.AuditEventDateTimeUTC.ToString() + "\t" + audit.AuditAction.ToString() + "\t\t" + audit.EntityName +"\n"+origObj, "\n"+newObj);
+                    //document.AddRowUsingParams(origObj, newObj);
+                }
+            }
+
+            var result = new FileInfo();
+            using (var stream = document.GenerateDocument(false))
+            {
+                result.Data = stream.ToArray();
+                result.FileName = document.DocumentTitle;
+                result.MimeType = "application/pdf";
+            }
+
+            return result;
+        }
+
+        public override EventNumber GetId()
+        {
+            return EventNumber.CreateAuditReport;
+        }
+    }
+}
