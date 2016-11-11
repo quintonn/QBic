ca936e9c651dad5b07980b6addae8f06dfeb0eb1
Quintonn <q10athome@gmail.com>
2016-11-11 15:38:52 +0400

    Fixed bugs

--- WebsiteTemplate/Backend/Processing/InputEventProcessor.cs
+++ WebsiteTemplate/Backend/Processing/InputEventProcessor.cs
@@ -51,6 +51,10 @@ namespace WebsiteTemplate.Backend.Processing
             await eventItem.Initialize(formData); //TODO: Do i need to call this here???? WHY!! If it's a must, maybe i should pass a flag that it's not the real initialize.
             foreach (var inputField in eventItem.GetInputFields())
             {
+                if (inputField is FileInput)
+                {
+                    Console.WriteLine("XX");
+                }
                 var value = inputField.GetValue(jsonData.GetValue<JToken>(inputField.InputName));
                 processedFormData.Add(inputField.InputName, value);
             }
--- WebsiteTemplateCore/nuget publish.bat
+++ WebsiteTemplateCore/nuget publish.bat
@@ -4,7 +4,7 @@ rem -Exclude "config\*.*"
 ECHO Packing Nuget Package
 nuget pack -OutputDirectory "..\..\..\NugetSource"  "..\WebsiteTemplateCore.csproj"
 ECHO Publishing Nuget Package
-nuget push "..\..\..\NugetSource\WebsiteTemplateCore*.nupkg" -s http://nugetrepo.q10hub.com/nuget v4J[TT?}eG_}+7rSg;r-AcGbhjH+F
+nuget push "..\..\..\NugetSource\WebsiteTemplateCore*.nupkg" -s http://nugetrepo.q10hub.com/7c8fb305-b447-4e6f-86dd-88a39d99b47c v4J[TT?}eG_}+7rSg;r-AcGbhjH+F
 ECHO Deleting Nuget Package
 del ..\..\..\NugetSource\WebsiteTemplateCore*.nupkg
 ECHO Done with nuget code
