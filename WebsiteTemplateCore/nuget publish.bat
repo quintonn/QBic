45d997f524fcb0a4593bf6874c20166b12097118
Quintonn <q10athome@gmail.com>
2016-11-11 14:08:35 +0400

    Updated code to publish nuget package to private repository

--- WebsiteTemplateCore/nuget publish.bat
+++ WebsiteTemplateCore/nuget publish.bat
@@ -1,5 +1,10 @@
 rem if not exist ..\NugetPackages mkdir ..\NugetPackages
 rem nuget pack -OutputDirectory "..\NugetPackages"  "..\WebsiteTemplateCore.csproj"
 rem -Exclude "config\*.*"
+ECHO Packing Nuget Package
 nuget pack -OutputDirectory "..\..\..\NugetSource"  "..\WebsiteTemplateCore.csproj"
-rem nuget push "..\NugetPackages\WebsiteTemplate*.nupkg" -s http://cloudup.azurewebsites.net/ CF8F5B76-5C89-4032-AC36-B1C6DB70CA67
+ECHO Publishing Nuget Package
+nuget push "..\..\..\NugetSource\WebsiteTemplateCore*.nupkg" -s http://nugetrepo.q10hub.com/nuget v4J[TT?}eG_}+7rSg;r-AcGbhjH+F
+ECHO Deleting Nuget Package
+del ..\..\..\NugetSource\WebsiteTemplateCore*.nupkg
+ECHO Done with nuget code
