913fcbac9545ad80f869fab0625968909334c55c
Quintonn <q10athome@gmail.com>
2016-12-30 11:25:25 +0400

    Remove acme challenge for now

--- WebsiteTemplate/Controllers/AcmeController.cs
+++ WebsiteTemplate/Controllers/AcmeController.cs
@@ -5,13 +5,13 @@ namespace WebsiteTemplate.Controllers
     [RoutePrefix(".well-known/acme-challenge")]
     public class AcmeController : ApiController
     {
-        [HttpGet]
-        [Route("{*path}")]
-        [AllowAnonymous]
-        public IHttpActionResult Test(string path)
-        {
-            //TODO: Can create class and input screen to manage this
-            return Json("successs: " + path);
-        }
+        //[HttpGet]
+        //[Route("{*path}")]
+        //[AllowAnonymous]
+        //public IHttpActionResult Test(string path)
+        //{
+        //    //TODO: Can create class and input screen to manage this
+        //    return Json("successs: " + path);
+        //}
     }
 }
