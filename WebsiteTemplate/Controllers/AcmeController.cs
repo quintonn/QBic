01e7c89c48ca400ed1e396a285ea7348b7a8429d
Quintonn <q10athome@gmail.com>
2017-12-19 22:47:51 +0400

    Change ACME code to allow normal ways of getting the ACME challenge values

--- WebsiteTemplate/Controllers/AcmeController.cs
+++ WebsiteTemplate/Controllers/AcmeController.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Net.Http;
 using System.Net;
+using WebsiteTemplate.Utilities;
+using System.IO;
 
 namespace WebsiteTemplate.Controllers
 {
@@ -18,9 +20,24 @@ namespace WebsiteTemplate.Controllers
             var request = System.Web.HttpContext.Current.Request.Url.ToString();
             var challenge = request.Split("/".ToCharArray()).Last();
 
+            var resp = challenge + "." + ChallengeResponse;
+            if (ChallengeResponse == "xxx")
+            {
+                var dir = XXXUtils.GetCurrentDirectory();
+                var physicallPath = dir + "\\.well-known\\acme-challenge\\" + challenge;
+                if (File.Exists(physicallPath))
+                {
+                    resp = File.ReadAllText(physicallPath);
+                }
+                else
+                {
+                    resp = string.Format("Path {0} does not exist", physicallPath);
+                }
+            }
+
             var response = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                Content = new StringContent(challenge + "." + ChallengeResponse)
+                Content = new StringContent(resp)
             };
 
             return ResponseMessage(response);
