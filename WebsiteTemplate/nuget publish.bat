f9a238b66efa073f99b425ce31d4476736fa4cc1
Quintonn <q10athome@gmail.com>
2018-10-11 14:18:35 +0300

    Fixed nuget publishing

--- WebsiteTemplate/nuget publish.bat
+++
@@ -1,5 +0,0 @@
-if not exist ..\NugetPackages mkdir ..\NugetPackages
-nuget pack -OutputDirectory "..\NugetPackages"  "..\WebsiteTemplate.csproj"
-rem -Exclude "config\*.*"
-nuget pack -OutputDirectory "..\..\..\NugetSource"  "..\WebsiteTemplate.csproj"
-rem nuget push "..\NugetPackages\WebsiteTemplate*.nupkg" -s http://cloudup.azurewebsites.net/ CF8F5B76-5C89-4032-AC36-B1C6DB70CA67
