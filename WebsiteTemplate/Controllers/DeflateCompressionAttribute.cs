f239abf802be03071d7ab3ce8358afedd6ecdf12
Quintonn <q10athome@gmail.com>
2019-02-08 19:58:30 +0200

    Fix deflate complression to copy original headers

--- WebsiteTemplate/Controllers/DeflateCompressionAttribute.cs
+++ WebsiteTemplate/Controllers/DeflateCompressionAttribute.cs
@@ -23,8 +23,26 @@ namespace WebsiteTemplate.Controllers
             var zlibbedContent = bytes == null ? new byte[0] : CompressionHelper.DeflateByte(bytes);
             actContext.Response.Content = new ByteArrayContent(zlibbedContent);
             actContext.Response.Content.Headers.Remove("Content-Type");
-            actContext.Response.Content.Headers.Add("Content-encoding", "deflate");
+
             //TODO: We're losing original headers, need to copy them.
+            foreach (var item in actContext.Response.Content.Headers)
+            {
+                if (item.Key == "Content-Type")
+                {
+                    continue;
+                }
+                //else if (item.Key == "ETag")
+                //{
+                //    actContext.Response.Headers.ETag = new System.Net.Http.Headers.EntityTagHeaderValue(item.Value.ToString());
+                //}
+                else
+                {
+                    actContext.Response.Content.Headers.Add(item.Key, item.Value);
+                }
+                
+            }
+            actContext.Response.Content.Headers.Add("Content-encoding", "deflate");
+           
             
             actContext.Response.Content.Headers.Add("Content-Type", originalType?.FirstOrDefault());
             base.OnActionExecuted(actContext);
