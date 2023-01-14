f9a7a1e0ce915596e3911fb323dc7d60f0fdde8e
Quintonn <q10athome@gmail.com>
2023-01-14 13:16:07 +0200

    Add more error logging

--- WebsiteTemplate/Backend/Processing/CoreProcessor.cs
+++ WebsiteTemplate/Backend/Processing/CoreProcessor.cs
@@ -51,7 +51,14 @@ namespace WebsiteTemplate.Backend.Processing
             catch (Exception error)
             {
                 var message = error.Message;
-                SystemLogger.LogError("Error in core processor during Process", this.GetType(), error);
+                SystemLogger.LogError("Error in core processor during Process: " + error.Message + "\r\n"+error.StackTrace, this.GetType(), error);
+                var tmp = error;
+                while (tmp.InnerException != null)
+                {
+                    tmp = tmp.InnerException;
+                    SystemLogger.LogError("\t"+tmp.Message+"\r\n"+tmp.StackTrace, this.GetType(), tmp);
+                }
+                
                 return new BadRequestObjectResult(JsonSerializer.Serialize(SystemLogger.GetMessageStack(error)));
                 //return new BadRequestErrorMessageResult(SystemLogger.GetMessageStack(error), new DefaultContentNegotiator(), requestMessage, new List<MediaTypeFormatter>()
                 //{
