9699286f5f1495e98fc945cdb6ac33a338f6ffea
Quintonn <q10athome@gmail.com>
2024-06-04 19:12:46 +0100

    manu changes to make view work with paging

--- QBic.FrontEnd_New/src/Components/View/ViewActionColumn.tsx
+++ QBic.FrontEnd_New/src/Components/View/ViewActionColumn.tsx
@@ -1,23 +1,29 @@
 import { Box, Button, SpaceBetween } from "@cloudscape-design/components";
 import {
   ColumnType,
+  MenuDetail,
   ViewColumn,
+  useMenu,
 } from "../../ContextProviders/MenuProvider/MenuProvider";
 import { showColumn } from "../../Utilities/viewUtils";
 
 export interface ViewActionColumnProps {
   rowData: any[];
   columns: ViewColumn[];
+  menu: MenuDetail;
 }
 
 const ColButton = ({
   rowData,
   column,
+  menu,
 }: {
   rowData: any;
   column: ViewColumn;
+  menu: MenuDetail;
 }) => {
   const visible = showColumn({ rowData, column });
+  const { onMenuClick } = useMenu();
 
   let label = column.ColumnLabel;
   if (
@@ -39,6 +45,24 @@ const ColButton = ({
         variant={
           column.ColumnType == ColumnType.Link ? "inline-link" : "normal"
         }
+        onClick={() => {
+          const id = rowData[column.KeyColumn];
+          const params = column.ParametersToPass || {};
+          params["ViewId"] = column.EventNumber;
+          params["RowId"] = id;
+
+          var formData = {
+            Id: id,
+            data: rowData,
+            viewSettings: "", // Why is this not included in the call?
+            //parameters: theColumn.ParametersToPass,
+            parameters: params,
+            eventParameters: menu.EventParameters,
+          };
+
+          // execute ui action
+          onMenuClick(column.EventNumber, formData);
+        }}
         wrapText={false}
         disabled={visible ? false : true} // this is so the cursor is normal when it's invisible
       >
@@ -51,6 +75,7 @@ const ColButton = ({
 export const ViewActionColumn = ({
   rowData,
   columns,
+  menu,
 }: ViewActionColumnProps) => {
   if (!columns || columns.length == 0) {
     return "";
@@ -62,7 +87,7 @@ export const ViewActionColumn = ({
         {columns
           // .filter((c) => showColumn({ rowData, column: c }))
           .map((c, i) => (
-            <ColButton key={i} rowData={rowData} column={c} />
+            <ColButton key={i} rowData={rowData} column={c} menu={menu} />
           ))}
         {/* <Button
         href="#"
