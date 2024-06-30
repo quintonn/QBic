c2fca1b13c3e50e0938d69b4a0deb85d08a4ea3e
Quintonn <q10athome@gmail.com>
2024-06-30 12:05:47 +0100

    clean up

--- QBic.FrontEnd_New/src/Components/MainBody/MainBody.component.tsx
+++ QBic.FrontEnd_New/src/Components/MainBody/MainBody.component.tsx
@@ -1,10 +1,4 @@
-import { useEffect, useState } from "react";
-import {
-  Outlet,
-  RouteObject,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from "../Home/Home.component";
 import { MainAppLayout } from "../MainAppLayout/MainAppLayout.component";
 import { Login } from "../Login/Login.component";
@@ -17,23 +11,14 @@ import { Provider } from "react-redux";
 import { FormComponent } from "../Form/Form.component";
 import { ActionProvider } from "../../ContextProviders/ActionProvider/ActionProvider";
 
+const ROUTES = [
+  { path: "/*", element: <Home /> },
+  { path: "/view/*", element: <ViewComponent /> },
+  { path: "/form/*", element: <FormComponent /> },
+  { path: "/login", element: <Login /> },
+];
+
 export const MainBody = () => {
-  const [routes, setRoutes] = useState<RouteObject[]>([]);
-  const [loadingRoutes, setLoadingRoutes] = useState(true);
-
-  routes.push({ path: "/*", element: <Home /> });
-  routes.push({ path: "/view/*", element: <ViewComponent /> });
-  routes.push({ path: "/form/*", element: <FormComponent /> });
-  routes.push({ path: "/login", element: <Login /> });
-
-  useEffect(() => {
-    //TODO: here we can fetch the user's menu
-    setTimeout(() => {
-      setRoutes([...routes, { path: "/test", element: <h1>Test</h1> }]);
-      setLoadingRoutes(false);
-    }, 10);
-  }, []);
-
   const router = createBrowserRouter([
     {
       element: (
@@ -49,13 +34,8 @@ export const MainBody = () => {
           </ModalProvider>
         </AuthProvider>
       ),
-      children: routes,
+      children: ROUTES,
     },
   ]);
-
-  if (!loadingRoutes) {
-    return <RouterProvider router={router} />;
-  }
-
-  return <h1>Loading...</h1>;
+  return <RouterProvider router={router} />;
 };
