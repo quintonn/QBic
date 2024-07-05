c1bbccb3870e4ae2e161c6c6ec32f0124796ea66
Quintonn <q10athome@gmail.com>
2024-07-05 18:41:59 +0100

    fix default error checking and some core error settings

--- WebsiteTemplate/Menus/InputItems/InputField.cs
+++ WebsiteTemplate/Menus/InputItems/InputField.cs
@@ -19,7 +19,7 @@ namespace WebsiteTemplate.Menus.InputItems
 
         public bool Mandatory { get; set; }
 
-        public List<Condition> MandatoryConditions { get; set; }
+        //public List<Condition> MandatoryConditions { get; set; } // doesn't seem to be used anywhere (new UI doesn't handle this yet)
 
         public bool RaisePropertyChangedEvent { get; set; }
 
@@ -32,7 +32,7 @@ namespace WebsiteTemplate.Menus.InputItems
             Mandatory = mandatory;
 
             VisibilityConditions = new List<Condition>();
-            MandatoryConditions = new List<Condition>();
+            //MandatoryConditions = new List<Condition>();
         }
 
         public abstract object GetValue(JToken jsonToken);
